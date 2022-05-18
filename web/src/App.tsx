acfd579a1f07a703f694767b01f7571529ee03e8
AglailsonSantiago <aglailsonsantiago@gmail.com>
2022-05-18 16:38:40 -0300

    Criação e personalização do button Feedback.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,5 @@
-interface ButtonProps{
-  text?: string;
-}
+import { Widget } from "./components/Widget";
 
-function Button(props: ButtonProps){
-  return <button className="bg-purple-800 p-2">{props.text ?? 'Default'}</button>
+export function App(){
+  return <Widget/>
 }
-
-function App() {
-  return <h1>
-    <Button text="Enviar"/>
-    <Button text="Salvar"/>
-    <Button />
-  </h1>
-}
-
-export default App
