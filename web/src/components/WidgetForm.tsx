8dae8be321e7e147f518d05fe7b19b515cc5e0e0
AglailsonSantiago <aglailsonsantiago@gmail.com>
2022-05-18 18:02:26 -0300

    Criando componente WidgetForm.

---
+++ web/src/components/WidgetForm.tsx
@@ -0,0 +1,7 @@
+export function WidgetForm(){
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl flex flex-col items-center shadow-lg">
+            Hello World!
+        </div>
+    )
+}
