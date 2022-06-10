573a011a9d334159ea921130538d8cf2767b70ba
AglailsonSantiago <aglailsonsantiago@gmail.com>
2022-06-09 22:21:23 -0300

    Ajustando botão de Screenshot.

---
+++ web/src/components/WidgetForm/ScreenshotButton.tsx
@@ -0,0 +1,41 @@
+import { Camera } from "phosphor-react";
+import html2canvas from 'html2canvas';
+import { useState } from "react";
+import { Loading } from "../Loading";
+
+interface ScreenshotButtonProps {
+    screenshot : string;
+    onScreenshotTook: (screenshot : string) => void;
+}
+
+export function ScreenshotButton({ screenshot, onScreenshotTook } : ScreenshotButtonProps){
+
+    const [isTakingScreenshot, setIsTakingScreenshot] = useState(false);
+
+    async function handleTakeScreenshot(){
+
+        setIsTakingScreenshot(true);
+
+        const canvas = await html2canvas(document.querySelector('html')!);
+        const base64image = canvas.toDataURL('image/png');
+
+        onScreenshotTook(base64image);
+        setIsTakingScreenshot(false);
+    }
+
+    if(screenshot){
+        return(
+            <p>Foto</p>
+        )
+    }
+
+    return (
+        <button
+            type="button"
+            onClick={handleTakeScreenshot}
+            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+        >
+            { isTakingScreenshot ? <Loading/> : <Camera className="w-6 h-6" /> }
+        </button>
+    )
+}
--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
 import { ArrowLeft, Camera } from "phosphor-react"
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
@@ -8,6 +10,8 @@ interface FeedbackContentStepProps {
 }
 
 export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps){
+    const [screenshot, setScreenshot] = useState<string | null>(null); 
+
     const feedbackTypeInfo = feedbackTypes[feedbackType]
     
     return (
@@ -34,12 +38,11 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                 />
 
                 <footer className="flex gap-2 mt-2">
-                    <button
-                        type="button"
-                        className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
-                    >
-                        <Camera className="w-6 h-6"/>
-                    </button>
+                    
+                    <ScreenshotButton
+                        screenshot={screenshot}
+                        onScreenshotTook = {setScreenshot}
+                    />
 
                     <button
                         type="submit"
